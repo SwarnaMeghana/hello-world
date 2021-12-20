ae0f2b2836b071feeead8b1c8f69789fc9c39845
SwarnaMeghana <96186699+SwarnaMeghana@users.noreply.github.com>
2021-12-20 10:57:53 +0530

    Add files via upload

---
+++ CreateObject.java
@@ -0,0 +1,12 @@
+class CreateObject 
+{    
+void show()    
+{    
+System.out.println("Welcome to java");    
+}    
+public static void main(String[] args)   
+{    
+CreateObject obj = new CreateObject();   
+obj.show();    
+}    
+}
